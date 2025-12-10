df676ece33e1a2a387ba3ae4d65c559995951287
Vinothgopi G <vinothgopi@hotmail.com>
2025-12-10 21:51:01 +0530

    update

---
+++ Algorithm_Problems/TwoCode.java
@@ -0,0 +1,27 @@
+package Algorithm_Problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoCode {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
